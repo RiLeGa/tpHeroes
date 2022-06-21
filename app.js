d2b2fcb553f8313e03bca6fc5fccf7cabfbcea6e
Rilega <leandrorioga@gmail.com>
2022-06-21 02:17:30 -0300

    modificacion de todo el css

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const port = 3030
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "views", "index.html")))
-app.get("/turing", (req,res) => res.sendFile(path.resolve(__dirname, "views", "turing.html")))
-app.get("/clarke", (req,res) => res.sendFile(path.resolve(__dirname, "views", "clarke.html")))
-app.get("/hooper", (req,res) => res.sendFile(path.resolve(__dirname, "views", "hooper.html")))
-
 app.listen(port, ()=> console.log(`Se levanto el servidor con exito en el http://localhost:${port}`))
+
+app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "views", "index.html")))
+app.get("/babbage", (req,res) => res.sendFile(path.resolve(__dirname, "views", "babbage.html")))
+app.get("/berner-lee", (req,res) => res.sendFile(path.resolve(__dirname, "views", "berner-lee.html")))
+app.get("/clarke", (req,res) => res.sendFile(path.resolve(__dirname, "views", "clarke.html")))
+app.get("/hamilton", (req,res) => res.sendFile(path.resolve(__dirname, "views", "hamilton.html")))
+app.get("/hooper", (req,res) => res.sendFile(path.resolve(__dirname, "views", "hooper.html")))
+app.get("/lovelace", (req,res) => res.sendFile(path.resolve(__dirname, "views", "lovelace.html")))
+app.get("/turing", (req,res) => res.sendFile(path.resolve(__dirname, "views", "turing.html")))
